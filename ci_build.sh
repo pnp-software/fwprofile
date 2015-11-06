2d9506f660f49f1e6906fb79c98537badd969e49
Vaclav Cechticky <vaclav.cechticky@pnp-software.com>
2015-11-06 15:17:58 +0100

    scons setup fixed

--- ci_build.sh
+++ ci_build.sh
@@ -1,15 +1,5 @@
 #!/usr/bin/env bash
 
-set -x
+env = Environment()
+env.Program('fwprofile', ['./src/*.c'])
 
-if [ $BUILD_TYPE == "default" ]; then
-    mkdir tmp
-    BUILD_PREFIX=$PWD/tmp
-
-    CONFIG_OPTS=()
-    
-    # Build, check, and install fwprofile from local source
-    (./autogen.sh && ./configure "${CONFIG_OPTS[@]}" && make check-verbose VERBOSE=1 && make install) || exit 1
-else
-    cd ./builds/${BUILD_TYPE} && ./ci_build.sh
-fi
