cc063aaade9f5beb4c2dc0f237dad0fe7e5c33c9
Marcel Opprecht <marcel.opprecht@pnp-software.com>
2016-11-03 10:33:15 +0100

    Fixed stylings in FwPrConfig.c

--- src/FwPrConfig.c
+++ src/FwPrConfig.c
@@ -29,28 +29,19 @@
 #include <stdlib.h>
 
 /**
- * Create a control flow (other than the control flow from the initial node)
- * with the
+ * Create a control flow (other than the control flow from the initial node) with the
  * given characteristics and add it to state machine.
- * The newly created control flow is stored in the control flow array at the
- * first non-null location
- * (in other words, the first control flow to be added to the state machine is
- * stored at location 0,
+ * The newly created control flow is stored in the control flow array at the first non-null location
+ * (in other words, the first control flow to be added to the state machine is stored at location 0,
  * the second control flow is stored at location 1, etc).
- * @param prDesc the descriptor of the state machine to which the control flow
- * is added.
+ * @param prDesc the descriptor of the state machine to which the control flow is added.
  * @param srcId the identifier of the source of the control flow.
  * @param srcType the type of the source.
- * @param destId the identifier of the destination of the control flow. If the
- * destination is an action
- * node, this argument has a positive value equal to the destination identifier.
- * If the destination
- * is a decision node, this argument has a negative value equal to the opposite
- * of the destination
- * identifier. If the destination is the final node, this argument has the value
- * zero.
- * @param cfGuard the control flow guard (or NULL if no guard is associated to
- * this control flow).
+ * @param destId the identifier of the destination of the control flow. If the destination is an action
+ * node, this argument has a positive value equal to the destination identifier. If the destination
+ * is a decision node, this argument has a negative value equal to the opposite of the destination
+ * identifier. If the destination is the final node, this argument has the value zero.
+ * @param cfGuard the control flow guard (or NULL if no guard is associated to this control flow).
  */
 static void AddFlow(FwPrDesc_t prDesc, FwPrCounterS1_t srcId, NodeType_t srcType, FwPrCounterS1_t destId,
                     FwPrGuard_t cfGuard);
@@ -59,21 +50,17 @@ static void AddFlow(FwPrDesc_t prDesc, FwPrCounterS1_t srcId, NodeType_t srcType
  * Add an action to the set of actions in the procedure descriptor.
  * This function scans the array of actions in the procedure descriptor and
  * can then have one of three outcomes:
- * - if the argument action is already present in the array of actions, the
- * function
+ * - if the argument action is already present in the array of actions, the function
  *   returns the index of the location where the action is stored;
- * - if the argument action is not present and the array of actions is not full,
- * the
+ * - if the argument action is not present and the array of actions is not full, the
  *   function adds it at the first free location and returns the index of this
  *   location;
- * - if the argument action is not present but the array of actions is already
- * full,
+ * - if the argument action is not present but the array of actions is already full,
  *   the function returns 0.
  * .
  * Note that the argument action is guaranteed to be different from NULL
  * (because function <code>::FwPrAddAction</code> handles the case of action
- * being equal to NULL and, in that case, it does not call
- * <code>AddAction</code>.
+ * being equal to NULL and, in that case, it does not call <code>AddAction</code>.
  * @param prDesc descriptor of the procedure where the action is added
  * @param action action to be added
  * @return the location where the action is stored in the array or -1 if the
@@ -86,15 +73,12 @@ static FwPrCounterS1_t AddAction(FwPrDesc_t prDesc, FwPrAction_t action);
  * This function scans the array of guards in the procedure descriptor and
  * can then have one of three outcomes:
  * - if the argument guard is equal to NULL, the function returns 0;
- * - if the argument guard is already present in the array of guards, the
- * function
+ * - if the argument guard is already present in the array of guards, the function
  *   returns the index of the location where the guard is stored;
- * - if the argument guard is not present and the array of guards is not full,
- * the
+ * - if the argument guard is not present and the array of guards is not full, the
  *   function adds it at the first free location and returns the index of this
  *   location;
- * - if the argument guard is not present but the array of guards is already
- * full, the
+ * - if the argument guard is not present but the array of guards is already full, the
  *   function returns 0.
  * @param prDesc descriptor of the procedure where the guard is added
  * @param guard guard to be added
@@ -103,20 +87,17 @@ static FwPrCounterS1_t AddAction(FwPrDesc_t prDesc, FwPrAction_t action);
  */
 static FwPrCounterS1_t AddGuard(FwPrDesc_t prDesc, FwPrGuard_t guard);
 
-/* -----------------------------------------------------------------------------------------------------------------
- */
+/* ----------------------------------------------------------------------------------------------------------------- */
 void FwPrSetData(FwPrDesc_t prDesc, void* prData) {
   prDesc->prData = prData;
 }
 
-/* -----------------------------------------------------------------------------------------------------------------
- */
+/* ----------------------------------------------------------------------------------------------------------------- */
 void* FwPrGetData(FwPrDesc_t prDesc) {
   return prDesc->prData;
 }
 
-/* -----------------------------------------------------------------------------------------------------------------
- */
+/* ----------------------------------------------------------------------------------------------------------------- */
 void FwPrAddActionNode(FwPrDesc_t prDesc, FwPrCounterS1_t nodeId, FwPrAction_t action) {
 
   PrANode_t*    aNode;
@@ -158,8 +139,7 @@ void FwPrAddActionNode(FwPrDesc_t prDesc, FwPrCounterS1_t nodeId, FwPrAction_t a
   return;
 }
 
-/* -----------------------------------------------------------------------------------------------------------------
- */
+/* ----------------------------------------------------------------------------------------------------------------- */
 void FwPrAddDecisionNode(FwPrDesc_t prDesc, FwPrCounterS1_t nodeId, FwPrCounterS1_t nOfOutFlows) {
   PrDNode_t*    dNode;
   PrBaseDesc_t* prBase = prDesc->prBase;
@@ -200,56 +180,47 @@ void FwPrAddDecisionNode(FwPrDesc_t prDesc, FwPrCounterS1_t nodeId, FwPrCounterS
   return;
 }
 
-/* -----------------------------------------------------------------------------------------------------------------
- */
+/* ----------------------------------------------------------------------------------------------------------------- */
 void FwPrAddFlowIniToAct(FwPrDesc_t prDesc, FwPrCounterS1_t destId, FwPrGuard_t cfGuard) {
   AddFlow(prDesc, 0, stoppedNode, destId, cfGuard);
 }
 
-/* -----------------------------------------------------------------------------------------------------------------
- */
+/* ----------------------------------------------------------------------------------------------------------------- */
 void FwPrAddFlowIniToDec(FwPrDesc_t prDesc, FwPrCounterS1_t destId, FwPrGuard_t cfGuard) {
   AddFlow(prDesc, 0, stoppedNode, (FwPrCounterS1_t)(-destId), cfGuard);
 }
 
-/* -----------------------------------------------------------------------------------------------------------------
- */
+/* ----------------------------------------------------------------------------------------------------------------- */
 void FwPrAddFlowActToAct(FwPrDesc_t prDesc, FwPrCounterS1_t srcId, FwPrCounterS1_t destId, FwPrGuard_t cfGuard) {
   AddFlow(prDesc, srcId, actionNode, destId, cfGuard);
 }
 
-/* -----------------------------------------------------------------------------------------------------------------
- */
+/* ----------------------------------------------------------------------------------------------------------------- */
 void FwPrAddFlowActToDec(FwPrDesc_t prDesc, FwPrCounterS1_t srcId, FwPrCounterS1_t destId, FwPrGuard_t cfGuard) {
   AddFlow(prDesc, srcId, actionNode, (FwPrCounterS1_t)(-destId), cfGuard);
 }
 
-/* -----------------------------------------------------------------------------------------------------------------
- */
+/* ----------------------------------------------------------------------------------------------------------------- */
 void FwPrAddFlowDecToAct(FwPrDesc_t prDesc, FwPrCounterS1_t srcId, FwPrCounterS1_t destId, FwPrGuard_t cfGuard) {
   AddFlow(prDesc, srcId, decisionNode, destId, cfGuard);
 }
 
-/* -----------------------------------------------------------------------------------------------------------------
- */
+/* ----------------------------------------------------------------------------------------------------------------- */
 void FwPrAddFlowDecToDec(FwPrDesc_t prDesc, FwPrCounterS1_t srcId, FwPrCounterS1_t destId, FwPrGuard_t cfGuard) {
   AddFlow(prDesc, srcId, decisionNode, (FwPrCounterS1_t)(-destId), cfGuard);
 }
 
-/* -----------------------------------------------------------------------------------------------------------------
- */
+/* ----------------------------------------------------------------------------------------------------------------- */
 void FwPrAddFlowActToFin(FwPrDesc_t prDesc, FwPrCounterS1_t srcId, FwPrGuard_t cfGuard) {
   AddFlow(prDesc, srcId, actionNode, 0, cfGuard);
 }
 
-/* -----------------------------------------------------------------------------------------------------------------
- */
+/* ----------------------------------------------------------------------------------------------------------------- */
 void FwPrAddFlowDecToFin(FwPrDesc_t prDesc, FwPrCounterS1_t srcId, FwPrGuard_t cfGuard) {
   AddFlow(prDesc, srcId, decisionNode, 0, cfGuard);
 }
 
-/* -----------------------------------------------------------------------------------------------------------------
- */
+/* ----------------------------------------------------------------------------------------------------------------- */
 static void AddFlow(FwPrDesc_t prDesc, FwPrCounterS1_t srcId, NodeType_t srcType, FwPrCounterS1_t destId,
                     FwPrGuard_t cfGuard) {
 
@@ -296,26 +267,19 @@ static void AddFlow(FwPrDesc_t prDesc, FwPrCounterS1_t srcId, NodeType_t srcType
     nOfOutFlows = 1;
   }
 
-  /* New control flow will be stored in the control flow array of the Procedure
-   * Descriptor at a location
-   * in the range [baseLoc, baseLoc+nOfOutFlows-1]. We check that this range of
-   * locations
-   * is not already full (note that the checks performed when a node is added to
-   * the procedure
-   * ensure that the location [baseLoc+nOfOutFlows-1] is within the range of
-   * flow array..
+  /* New control flow will be stored in the control flow array of the Procedure Descriptor at a location
+   * in the range [baseLoc, baseLoc+nOfOutFlows-1]. We check that this range of locations
+   * is not already full (note that the checks performed when a node is added to the procedure
+   * ensure that the location [baseLoc+nOfOutFlows-1] is within the range of flow array..
    */
   if (prBase->flows[baseLoc + nOfOutFlows - 1].iGuard != -1) {
     prDesc->errCode = prTooManyFlows;
     return;
   }
 
-  /* Identify location where newly added control flow will be stored in the
-   * control flow array.
-   * Note that the nOfOutFlows is guaranteed to be greater than zero by the way
-   * it is
-   * initialized in the previous statements in this function. Hence, the loop
-   * will be
+  /* Identify location where newly added control flow will be stored in the control flow array.
+   * Note that the nOfOutFlows is guaranteed to be greater than zero by the way it is
+   * initialized in the previous statements in this function. Hence, the loop will be
    * taken at least once. */
   for (i = 0; i < nOfOutFlows; i++)
     if (prBase->flows[baseLoc + i].iGuard == -1) {
@@ -333,8 +297,7 @@ static void AddFlow(FwPrDesc_t prDesc, FwPrCounterS1_t srcId, NodeType_t srcType
   return;
 }
 
-/* -----------------------------------------------------------------------------------------------------------------
- */
+/* ----------------------------------------------------------------------------------------------------------------- */
 static FwPrCounterS1_t AddAction(FwPrDesc_t prDesc, FwPrAction_t action) {
   FwPrCounterS1_t i;
 
@@ -354,8 +317,7 @@ static FwPrCounterS1_t AddAction(FwPrDesc_t prDesc, FwPrAction_t action) {
   return 0;
 }
 
-/* -----------------------------------------------------------------------------------------------------------------
- */
+/* ----------------------------------------------------------------------------------------------------------------- */
 static FwPrCounterS1_t AddGuard(FwPrDesc_t prDesc, FwPrGuard_t guard) {
   FwPrCounterS1_t i;
 
@@ -378,8 +340,7 @@ static FwPrCounterS1_t AddGuard(FwPrDesc_t prDesc, FwPrGuard_t guard) {
   return 0;
 }
 
-/* -----------------------------------------------------------------------------------------------------------------
- */
+/* ----------------------------------------------------------------------------------------------------------------- */
 FwPrErrCode_t FwPrCheck(FwPrDesc_t prDesc) {
 
   FwPrCounterS1_t i, j;
@@ -405,8 +366,7 @@ FwPrErrCode_t FwPrCheck(FwPrDesc_t prDesc) {
     if (prBase->flows[i].iGuard == -1)
       return prNullFlow;
 
-  /* Check that all control flow destinations are legal action nodes or decision
-   * nodes */
+  /* Check that all control flow destinations are legal action nodes or decision nodes */
   for (i = 0; i < prBase->nOfFlows; i++) {
     if (prBase->flows[i].dest > prBase->nOfANodes)
       return prIllegalADest;
@@ -451,8 +411,7 @@ FwPrErrCode_t FwPrCheck(FwPrDesc_t prDesc) {
   return prSuccess;
 }
 
-/* -----------------------------------------------------------------------------------------------------------------
- */
+/* ----------------------------------------------------------------------------------------------------------------- */
 void FwPrOverrideAction(FwPrDesc_t prDesc, FwPrAction_t oldAction, FwPrAction_t newAction) {
   FwPrCounterS1_t i;
 
@@ -470,8 +429,7 @@ void FwPrOverrideAction(FwPrDesc_t prDesc, FwPrAction_t oldAction, FwPrAction_t 
   prDesc->errCode = prUndefAction;
 }
 
-/* -----------------------------------------------------------------------------------------------------------------
- */
+/* ----------------------------------------------------------------------------------------------------------------- */
 void FwPrOverrideGuard(FwPrDesc_t prDesc, FwPrGuard_t oldGuard, FwPrGuard_t newGuard) {
   FwPrCounterS1_t i;
 
